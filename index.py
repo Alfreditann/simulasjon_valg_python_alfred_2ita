323c9562de8ed61e47dc635abddf1b740695cb71
Alfreditann <alula006@osloskolen.no>
2025-09-11 10:30:22 +0200

    Update index.py

--- index.py
+++ index.py
@@ -58,10 +58,10 @@ def kjør_valget():
     elif ki_stemme == 24 or 25 or 26 or 27 or 28:
         stemme["sv"] +=1
     elif ki_stemme == 29 or 30 or 31 or 32 or 33 or 34 or 35 or 36 or 37 or 38 or 39 or 40 or 41 or 42:
-        stemme ["høyre"]
+        stemme ["høyre"] +=1
     elif ki_stemme == 43 or 44 or 45 or 46 or 47 or 48 or 49 or 50 or 51 or 52 or 53 or 54 or 55 or 56 or 57 or 58 or 59 or 60 or 61 or 62 or 63 or 64 or 65 or 66:
-        stemme["frp"]
+        stemme["frp"] +=1
     elif ki_stemme == 67 or 68 or 69 or 70 or 71 or 72 or 73 or 74 or 75 or 76 or 77 or 78 or 79 or 80 or 81 or 82 or 83 or 84 or 85 or 86 or 87 or 88 or 89 or 90 or 91 or 92 or 93 or 94:
-        stemme ["ap"]
+        stemme ["ap"] +=1
 
 
